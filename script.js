98d14af3d99478ed6f32836586be3bae4f1ba2c8
Anant Jain <anantjai@usc.edu>
2018-12-30 12:51:29 -0800

    Fix cross-browser issues

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('div.caption').each(function(index) {
     if ($(this).css("-webkit-backdrop-filter") == null) {
-  		$(this).css({"background-color": "rgba(240, 240, 240, 0.95)"});
+  		$(this).css({"background-color": "#E6E6E2"});
   	}
   })
 })
